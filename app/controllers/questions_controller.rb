17d4dc4248fd9e256956e4b2e01102c92c6f678b
Birzhan Serik <180107167@stu.sdu.edu.kz>
2021-09-11 02:01:16 +0600

    file operations optimization & set_question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!, except: %w[index show]
-  before_action :authorize!, only: %w[update destroy]
+  before_action :set_question, only: %w[show edit update destroy]
+  before_action :authorize!, only: %w[edit update destroy]
 
   def index
     @questions = Question.all
@@ -9,11 +10,13 @@ class QuestionsController < ApplicationController
 
   def show
     @answer = Answer.new
-    @best_answer = question.best_answer
-    @answers = question.answers.where.not(id: @question.best_answer_id)
+    @best_answer = @question.best_answer
+    @answers = @question.answers.where.not(id: @question.best_answer_id)
   end
 
-  def new; end
+  def new
+    @question = current_user.questions.new
+  end
 
   def edit; end
 
@@ -28,27 +31,25 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    question.update(question_params)
+    @question.update(question_params)
   end
 
   def destroy
-    question.destroy
+    @question.destroy
     redirect_to questions_path, notice: 'Question was successfully deleted.'
   end
 
   private
 
-  def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+  def set_question
+    @question = Question.with_attached_files.find(params[:id])
   end
 
   def authorize!
-    if current_user.not_author_of? question
+    if current_user.not_author_of? @question
       redirect_to questions_path, notice: 'restricted access'
     end
   end
-
-  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body, files: [])

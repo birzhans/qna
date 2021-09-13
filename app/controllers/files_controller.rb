3d76624a8113fbe70d0c5f1f8d379aa93f0e8d50
Birzhan Serik <180107167@stu.sdu.edu.kz>
2021-09-13 22:59:48 +0600

    Update files_controller.rb

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,22 +1,13 @@
 class FilesController < ApplicationController
-  before_action :set_file
-  before_action :authorize!
-
   def destroy
-    @file.purge
-  end
-
-  private
-
-  def set_file
     @id = params[:id]
     @file = ActiveStorage::Attachment.find(@id)
     @record = @file.record
-  end
 
-  def authorize!
-    if current_user.id != @record.user_id
-      redirect_to root_path, notice: 'Restricted access'
+    if current_user.not_author_of?(@record)
+      return redirect_to root_path, notice: 'Restricted access'
     end
+
+    @file.purge
   end
 end
